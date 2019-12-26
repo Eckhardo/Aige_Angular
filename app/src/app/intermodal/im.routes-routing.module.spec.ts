b0a133b480982af456ec10181b88af2f1d063823
Eckhard Kirschning <eckhard.kirschning@freenet.de>
2019-12-26 08:18:58 +0100

    Add With Jamine/Karma Integration Tests

--- app/src/app/intermodal/im.routes-routing.module.spec.ts
+++ app/src/app/intermodal/im.routes-routing.module.spec.ts
@@ -18,7 +18,6 @@ import {appRoutes} from '../app-routing.module';
 import {AppComponent} from '../app.component';
 import {HomeComponent} from '../home/home/home.component';
 
-
 /***
  * Imports configs for root app routing module and child module intermodal routing module:
  *<ul> Tests check that
